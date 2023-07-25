7dbfeea0e8860ea7ec0ea5bace84d3e7bd824b36
Tiago Viana <tiagoviana.1003@gmail.com>
2023-07-25 16:53:28 +0100

    update

--- app/src/main/java/com/example/ao_arduinobt/ShowAllActivity.kt
+++ app/src/main/java/com/example/ao_arduinobt/ShowAllActivity.kt
@@ -1,14 +1,11 @@
 package com.example.ao_arduinobt
 
-import CustomAdapter
+import com.example.ao_arduinobt.Adapter.AllHistoryAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Adapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ao_arduinobt.RoomDB.History
-import com.example.ao_arduinobt.RoomDB.HistoryAplication
 
 class ShowAllActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +14,7 @@ class ShowAllActivity : AppCompatActivity() {
 
         var dataList = listOf(History(12f, 12f))
         val recyclerView = findViewById<RecyclerView>(R.id.recView)
-        val adapter = CustomAdapter(dataList)
+        val adapter = AllHistoryAdapter(dataList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
