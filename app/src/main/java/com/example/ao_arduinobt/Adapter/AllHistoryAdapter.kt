cc3cedf02c085a609fc3c20af1519cf7ff1abd12
Tiago Viana <tiagoviana.1003@gmail.com>
2023-07-25 21:26:33 +0100

    update recycler to the arquiteture of the room DB

--- app/src/main/java/com/example/ao_arduinobt/Adapter/AllHistoryAdapter.kt
+++ app/src/main/java/com/example/ao_arduinobt/Adapter/AllHistoryAdapter.kt
@@ -4,33 +4,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ao_arduinobt.R
 import com.example.ao_arduinobt.RoomDB.History
 
-class AllHistoryAdapter(private val dataList: List<History>) :
-    RecyclerView.Adapter<AllHistoryAdapter.ViewHolder>() {
+class AllHistoryAdapter() : ListAdapter<History, AllHistoryAdapter.AllHistoryViewHolder>(WORDS_COMPARATOR) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllHistoryViewHolder {
+        return AllHistoryViewHolder.create(parent)
+    }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    override fun onBindViewHolder(holder: AllHistoryViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current)
+    }
+
+    class AllHistoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView1: TextView = itemView.findViewById(R.id.textDate)
         val textView2: TextView = itemView.findViewById(R.id.textTemp)
         val textView3: TextView = itemView.findViewById(R.id.textHumidity)
+
+        fun bind(elem: History) {
+            textView1.text = elem.date_time_measure.toString()
+            textView2.text = elem.temperature.toString()
+            textView3.text = elem.humidity.toString()
+        }
+        companion object {
+            fun create(parent: ViewGroup): AllHistoryViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recycler, parent, false)
+                return AllHistoryViewHolder(view)
+            }
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recycler, parent, false)
-        return ViewHolder(itemView)
-    }
+    companion object {
+        private val WORDS_COMPARATOR = object : DiffUtil.ItemCallback<History>() {
+            override fun areContentsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem == newItem
+            }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val data = dataList[position]
-        holder.textView3.text = data.humidity.toString()
-        holder.textView2.text = data.temperature.toString()
-        //holder.textView1.text = data.text3
-    }
-
-    override fun getItemCount(): Int {
-        return dataList.size
+            override fun areItemsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
 }
