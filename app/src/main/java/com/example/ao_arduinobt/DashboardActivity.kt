c5cb032cf343bca2a22c13e91d3cdac1504619b4
Gonçalo Fernandes <65426418+goncalomf@users.noreply.github.com>
2023-07-27 15:49:16 +0100

    Update

--- app/src/main/java/com/example/ao_arduinobt/DashboardActivity.kt
+++ app/src/main/java/com/example/ao_arduinobt/DashboardActivity.kt
@@ -2,9 +2,15 @@ package com.example.ao_arduinobt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import com.example.ao_arduinobt.RoomDB.HistoryAplication
+import com.example.ao_arduinobt.RoomDB.HistoryViewModel
+import com.example.ao_arduinobt.RoomDB.HistoryViewModelFactory
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
+import java.time.ZoneOffset
 
 class DashboardActivity : AppCompatActivity() {
     lateinit var lineGraphView: GraphView
@@ -18,22 +24,29 @@ class DashboardActivity : AppCompatActivity() {
         lineGraphView = findViewById(R.id.idGraphView)
 
 
+        val historyViewModel: HistoryViewModel by viewModels {
+            HistoryViewModelFactory((application as HistoryAplication).repository)
+        }
+
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries()
+
+        val history = historyViewModel.allHistory.observe(this) { history ->
+            history.let { data ->
+                data.forEach { dt ->
+                    series.appendData(
+                        DataPoint(
+                            dt.date_time_measure.toEpochSecond(ZoneOffset.UTC).toDouble(),
+                            dt.temperature.toDouble()
+                        ), true, 1
+                    )
+                }
+            }
+        }
+
+        Log.d("DEBUG", history.toString())
+
         // on below line we are adding data to our graph view.
 
-        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
-            arrayOf(
-
-                DataPoint(0.0, 1.0),
-                DataPoint(1.0, 3.0),
-                DataPoint(2.0, 4.0),
-                DataPoint(3.0, 9.0),
-                DataPoint(4.0, 6.0),
-                DataPoint(5.0, 3.0),
-                DataPoint(6.0, 6.0),
-                DataPoint(7.0, 1.0),
-                DataPoint(8.0, 2.0)
-            )
-        )
 
         // on below line adding animation
         lineGraphView.animate()
